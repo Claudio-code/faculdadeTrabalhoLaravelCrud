020f69237d1dcb42c7cf2e88b2a52b9151551c5a
claudio-code <suporte2424@outlook.com>
2020-03-21 19:11:04 -0300

    bootstrap

---
+++ crudAluno/webpack.mix.js
@@ -0,0 +1,16 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/site/bootstrap.js')
+    .scripts('node_modules/bootstrap/dist/css/bootstrap.css', 'public/site/bootstrap/bootstrap.css');
